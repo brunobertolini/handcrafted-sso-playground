25f28eab3b279028d290ebe392ba1dbeb13c8304
Bruno Bertolini <dev@brunobertolini.com>
2024-09-24 17:51:21 -0300

    feat: avoid user access login/signup pages when logged

--- apps/idp/middleware.ts
+++ apps/idp/middleware.ts
@@ -1,1 +1,8 @@
-export { auth as middleware } from "./auth"
+import { auth } from "./auth"
+
+export default auth((req) => {
+  if (req.auth && ['/login', '/signup'].includes(req.nextUrl.pathname)) {
+    const newUrl = new URL("/", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+})
